bd682d4f05a2d454062f3d50449829cbbd3cdaf3
Julian Williams <julianthedev@gmail.com>
2019-11-05 16:57:55 -0700

    Added express. Building out frontend.

---
+++ src/header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div className="Header">
+            <div className="Header-logo"></div>
+            <div className="Header-menu">
+                <ul className="Header-nav">
+                    <li><a>Home</a></li>
+                    <li><a>Service</a></li>
+                    <li><a>Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default App;
