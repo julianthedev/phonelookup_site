114192bee12eb368356efc6c4e259b420bdc5bad
Julian Williams <julianthedev@gmail.com>
2019-11-22 16:35:15 -0700

    more css

--- src/Body1.js
+++ src/Body1.js
@@ -4,7 +4,7 @@ function Body1() {
     return (
         <div className="Body1-container">
             <div className="Body1-info-container">
-                <
+                
             </div>
         </div>
     );
